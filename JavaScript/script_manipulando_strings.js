22c7c0bcde7174a3ea0d88a5d2a7491de4b81fcf
Mariane de Freitas <mariane11.freitas@gmail.com>
2021-08-12 09:38:59 -0300

    manipulando_strings

---
+++ JavaScript/script_manipulando_strings.js
@@ -0,0 +1,38 @@
+// // 1 
+// var txt = "abcdefghijklmn";
+// var pos = txt.indexOf("h");
+// console.log(pos);
+
+// // 2
+// var txt = "Posso comer bananas o dia todo";
+// var x = txt.slice(txt.indexOf("b"),txt.indexOf("as")+1);
+// console.log(x);
+
+// // 3
+// var txt = "Olá, mundo";
+// txt = txt.replace("mundo","bem vindo");
+// console.log(txt);
+
+// // 4
+// var txt = "Olá, mundo";
+// txt = txt.toUpperCase();
+// console.log(txt);
+
+// // 5 
+// var txt = "Olá, mundo";
+// txt = txt.toLowerCase();
+// console.log(txt);
+
+// // 6
+// var txt = "Olá, mundo!";
+// var x = txt.length
+// alert(x);
+
+// // 7
+// var txt = "We are \"Vikings\"";
+// alert(txt);
+
+// // 8
+// var str1 = "Olá ";
+// var str2 = "Mundo!";
+// alert(str1 + str2);
